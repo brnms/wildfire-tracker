ae470d61dd2319432252803d8928c1f85bee792e
Bruno Santos <bruno.matasantos@gmail.com>
2021-02-12 12:01:55 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import { useState, useEffect } from 'react'
-import Map from'./components/Map'
+import Map from './components/Map'
+import Loader from './components/Loader'
+import Header from './components/Header'
 
 function App() {
   const [eventData, setEventData] = useState([])
-  const [loading, setLoading ] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    const fetchEvents = async () => {
-      setLoading(true)
-      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
-      const { events } = await res.json()
+      const fetchEvents = async() => {
+        setLoading(true)
+        const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events');
+        const { events } = await res.json();
 
-      setEventData(events)
-      setLoading(false)
-    }
+        setEventData(events)
+        setLoading(false)
+      }
 
-    fetchEvents()
+      fetchEvents()
   }, [])
 
   return (
-    <Map />
+    <div>
+      <Header />
+      { !loading ? <Map eventData={eventData} /> : <Loader />}
+    </div>
   );
 }
 
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,17 +1,27 @@
-import GoogleMapReact from 'google-map-react'
-import LocationMarker from './LocationMarker'
+import { useState } from 'react';
+import GoogleMapReact from 'google-map-react';
+import LocationMarker from './LocationMarker';
+import LocationInfoBox from './LocationInfoBox';
 
-const Map = ({ center, zoom }) => {
-    return (
+const Map = ({ eventData, center, zoom }) => {
+    const [locationInfo, setLocationInfo] = useState(null)
+
+    const markers = eventData.map(ev => {
+        if(ev.categories[0].id === 8) {
+            return <LocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} onClick={() => setLocationInfo({ id: ev.id, title: ev.title})}/>
+        }
+        return null
+    })
+    return(
         <div className="map">
             <GoogleMapReact
-                bootstrapURLKeys={{ key:
-                    'KEY' }}
+                bootstrapURLKeys={{key: 'AIzaSyAkDUtTNUB5knDq3fu8_aPVAWA_el0BYZw' }}
                 defaultCenter={ center }
-                defaultZoom={zoom}
+                defaultZoom={ zoom }
             >
-                <LocationMarker lat={center.lat} lng={center.lng} />
+                {markers}
             </GoogleMapReact>
+            {locationInfo && <LocationInfoBox info={locationInfo} />}
         </div>
     )
 }
@@ -19,7 +29,7 @@ const Map = ({ center, zoom }) => {
 Map.defaultProps = {
     center: {
         lat: 42.3265,
-        lng: -122.8756,
+        lng: -122.8756
     },
     zoom: 6
 }
